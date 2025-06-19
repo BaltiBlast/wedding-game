37b4cef7e6d194e8c8c6d98165d60d7b68748ec4
BaltiBlast <fougeray.florian@gmail.com>
2025-06-19 20:45:29 +0200

    adapte canvas for tynier screen

--- js/config.js
+++ js/config.js
@@ -5,6 +5,10 @@ const GameConfig = {
   type: Phaser.AUTO,
   parent: "game-container",
   backgroundColor: "#000000",
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
   physics: {
     default: "arcade",
     arcade: { debug: false },
